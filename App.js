eaaead3b2677a1afaf8a7cc568164f73e5b7410d
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-10-16 22:05:43 +0700

    oooo

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
+import React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+
+const Stack = createStackNavigator()
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Login"
+            >
+                <Stack.Screen name="Login" component={LoginScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView className="flex flex-1 justify-center items-center bg-blue-500">
+      <Text>LoginScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default LoginScreen
