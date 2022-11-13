b85334b0daf96a61048dd7ef7f0e5f40f9904127
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-11-13 23:39:47 +0700

    absen completedesign

--- constants/routes.js
+++ constants/routes.js
@@ -7,21 +7,20 @@ export default {
   HOME_TAB: 'Home Tab',
   HOME_DRAWER: 'Home Drawer',
 
-  ABSEN: 'Absen',
+  ABSEN: 'Absen Tab',
   ABSEN_TAB: 'Absen',
   ABSEN_DRAWER: 'Absen Drawer',
 
-  JADWAL: 'Jadwal',
+  JADWAL: 'Jadwal Tab',
   JADWAL_TAB: 'Jadwal',
   JADWAL_DRAWER: 'Jadwal Drawer',
 
   HISTORY: 'History',
-  HISTORY_TAB: 'History',
+  HISTORY_TAB: 'History Tab',
   HISTORY_DRAWER: 'History Drawer',
 
   SETTINGS: 'Settings',
-  SETTINGS_TAB: 'Settings',
-
+  SETTINGS_TAB: 'Settings Tab',
   SETTINGS_DETAIL: 'Settings Detail',
   PROFILE: 'My Profile',
 };
