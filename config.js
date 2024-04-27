f14b0476502316d31671190e3100875d8ad49450
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-04-27 20:42:39 +0700

    coba

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 
 
 
-const SERV = process.env.EXPO_PUBLIC_API_URL
+const SERV = 'https://xenter.my.id'
 const SERV_LOCAL = 'http://192.168.150.111:3507'
 
 const BASE = SERV + '/api'
 const BASE_LOCAL = SERV_LOCAL + '/api'
 
 // const PATH_IMG100 = 'https://rsudmsaleh.probolinggokota.go.id/simpeg/foto'
-// const PATH_IMG100 = 'http://36.89.103.114:4542/simpeg/foto'
-const PATH_IMG100 = process.env.EXPO_PUBLIC_PATH_IMG100
+const PATH_IMG100 = 'http://36.89.103.114:4542/simpeg/foto'
+// const PATH_IMG100 = process.env.EXPO_PUBLIC_PATH_IMG100
 
 
 
